2b5e2df61800dc9b7b9f46e26c1c1af986d07cd3
Archimidis Mertzanos <amertzanos@gmail.com>
2019-05-15 15:43:53 +0300

    First Version Commit

--- functions.php
+++ functions.php
@@ -1,4 +1,79 @@
 <?php
 /**
  * Core functions file of the theme.
+ * Χρησιμοποιούμε αυτό το αρχείο το οποίο διαβάζεται από το WordPress και
+ * σε αυτό δηλώνουμε functions για την παρουσίαση του theme , για παράδειγμα
+ * register navigation menus,
+ * register stylesheets and scripts
+ * functions που χρησιμοποιούνται παντού στο theme διότι το αρχείο αυτο είναι χρησιμοποιεί το autoload.
+ *
+ * Αν τώρα θέλετε να έχετε πολλά διαφορετικά theme related function μία καλή πρόταση είναι είτε να τις βάλετε σε
+ * ένα plugin είτε να τις διαχωρίσετε σε πολλά μικρά αρχεία.
+ *
+ * @url https://wpshout.com/wordpress-functions-php/
  */
+
+/*
+ * Δήλωση μεταβλητών constants για τα theme assets.
+ * Workshop Helper
+ * Κώδικας 1Α
+ */
+
+define('WC_THEME_CSS', trailingslashit(get_template_directory_uri()) . 'assets/css/');
+define('WC_THEME_JS', trailingslashit(get_template_directory_uri()) . 'assets/js/');
+
+/*
+ * Style enqueuing
+ * Workshop Helper
+ * Κώδικας 1B
+ */
+
+if (!function_exists('wc_styles')):
+    function wc_styles()
+    {
+        wp_enqueue_style('normalize', WC_THEME_CSS . 'normalize.css', '', '8.0.1', 'all');
+        wp_enqueue_style('flexboxgrid', WC_THEME_CSS . 'flexboxgrid.css', '', '1.0.0', 'all');
+        wp_enqueue_style('wc-style', get_stylesheet_uri(), '', time(), 'all');
+    }
+
+    add_action('wp_enqueue_scripts', 'wc_styles');
+endif;
+
+/*
+ * Script enqueuing
+ * Workshop Helper
+ * Κώδικας 1B
+ */
+if (!function_exists('wc_scripts')):
+    function wc_scripts()
+    {
+        wp_enqueue_script('wc-main-script', WC_THEME_JS . 'theme.js', array('jquery'), time(), true);
+    }
+
+    add_action('wp_enqueue_scripts', 'wc_scripts');
+endif;
+
+/*
+ * After setup theme
+ * Workshop Helper
+ * Κώδικας 1Γ
+ */
+
+if (!function_exists('wc_theme_setup')):
+    function wc_theme_setup()
+    {
+        add_theme_support( 'post-thumbnails' );
+        register_nav_menus(
+            array(
+                'wc-primary'    => __( 'Top Menu', 'wc' ),
+            )
+        );
+        add_theme_support( 'custom-logo', array(
+            'height'      => 50,
+            'width'       => 200,
+            'flex-height' => true,
+            'flex-width'  => true
+        ) );
+    }
+add_action('after_setup_theme','wc_theme_setup');
+endif;
--- header.php
+++ header.php
@@ -5,7 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?php wp_head(); ?>
+
+    <?php
+    /*
+     * Hook το οποίο δεν έχει κάποιες παραμέτρους και μπορούμε να δέσουμε πάνω του functions.
+     * Χρησιμοποιείται για να δέσουμε πάνω του scripts , css, js αλλά και για άλλες εργασίες.
+     * Τοποθετείται πάντα πριν το closing </head> tag. Είναι αναγκαίο στο theme μας.
+     */
+    wp_head();
+    ?>
 </head>
 
 <body <?php body_class();?>>
